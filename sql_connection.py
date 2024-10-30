359e6bbf80d7d7e0998d720a0cd155a1df56eedf
tyagirehan123 <143125376+tyagirehan123@users.noreply.github.com>
2024-10-30 14:05:05 +0530

    Add files via upload

--- sql_connection.py
+++ sql_connection.py
@@ -1,10 +1,14 @@
+import datetime
 import mysql.connector
-__cnx=None
+
+__cnx = None
+
 def get_sql_connection():
-    global __cnx
-    if __cnx is None:
-       __cnx= mysql.connector.connect(user='root', password='rehan',
-                              host='127.0.0.1',
-                              database='grocery_store')
-    return __cnx 
-    
+  print("Opening mysql connection")
+  global __cnx
+
+  if __cnx is None:
+    __cnx = mysql.connector.connect(user='root', password='rehan', database='grocery_store')
+
+  return __cnx
+
